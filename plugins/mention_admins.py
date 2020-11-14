0cc84497682fd857e81d932dfa312f0691e171f0
Alexey Razumov <alexey_razumov@tutanota.com>
2020-11-14 20:34:32 +0300

    uhhhhhh

---
+++ plugins/mention_admins.py
@@ -0,0 +1,18 @@
+from time import sleep
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.errors.exceptions.flood_420 import SlowmodeWait
+
+
+@Client.on_message(filters.command(['admin', 'admins'], '@'))
+def mention_admins(client: Client, message: Message):
+    message.delete()
+    message_text = message.text
+    for admin in client.iter_chat_members(message.chat.id, 0, '', 'administrators'):
+        message_text += f'<a href="tg://user?id={admin.user.id}">&#8203;</a>'
+    try:
+        client.send_message(message.chat.id, message_text)
+    except SlowmodeWait as e:
+        sleep(e.x)
+        client.send_message(message.chat.id, message_text)

aa79bf867722ab8f7d7809f8ce45bea0ac24f079
Alexey Razumov <alexey_razumov@tutanota.com>
2020-11-19 14:29:49 +0300

    It's Working!

--- plugins/purge.py
+++ plugins/purge.py
@@ -1,9 +1,18 @@
+from time import sleep
+
 from pyrogram import Client, filters
 from pyrogram.types import Message
+from pyrogram.errors import FloodWait
 
 
 @Client.on_message(filters.command('purge', ['.', '!']) & filters.me & filters.reply)
 def purge(client: Client, message: Message):
     messages = client.iter_history(message.chat.id, offset_id=message.reply_to_message.message_id, reverse=True)
     for message in messages:
-        client.delete_messages(message.chat.id, message.message_id)
+        try:
+            client.delete_messages(message.chat.id, message.message_id)
+        except FloodWait as e:
+            sleep(e.x)
+        except Exception as e:
+            message.edit("Unexpected error, can't continue...\n\n" + e)
+            return

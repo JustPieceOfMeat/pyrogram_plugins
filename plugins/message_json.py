b1580e8be6255cb40eb255821cd3cd35cb5f79bb
Alexey Razumov <alexey_razumov@tutanota.com>
2020-11-20 21:18:27 +0300

    The same thing we do every night, Pinky - try to take over the world!

--- plugins/message_json.py
+++ plugins/message_json.py
@@ -1,10 +1,22 @@
+import urllib.request, urllib.parse, urllib.error
+import urllib.request, urllib.error, urllib.parse
+from os import getlogin
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
 
-@Client.on_message(filters.command(['json', 'message'], ['.', '!']) & filters.me)
+def paste_code(content: str, author: str = getlogin()) -> str:  # from https://www.geany.org/p/help/api/
+    fields = [('content', content), ('author', author), ('lexer', 'json')]
+    encoded_data = urllib.parse.urlencode(fields).encode('utf-8')
+    request = urllib.request.Request(
+        'https://www.geany.org/p/api/',
+        encoded_data,
+    )
+    response = urllib.request.urlopen(request)
+    response_content = response.read()
+    return response_content.decode()
+
+
+@Client.on_message(filters.command(['json', 'message', 'src'], ['.', '!']) & filters.me)
 def json_cmd(client: Client, message: Message):
-    if 'here' in message.text:
-        message.reply_text(str(message.reply_to_message), parse_mode=None)
-    else:
-        client.send_message('me', str(message.reply_to_message), parse_mode=None)
+    message.edit(paste_code(str(message.reply_to_message), client.get_me().username or client.get_me().first_name))

5ead602a1657869e470f3bfa417427a77351701b
Alexey Razumov <alexey_razumov@tutanota.com>
2020-11-30 20:02:44 +0300

    hacky sack

--- plugins/pixiv.py
+++ plugins/pixiv.py
@@ -1,6 +1,6 @@
 from os import environ, path, remove
 import re
-from typing import List, Union
+from typing import List, Dict, Union
 import urllib.request
 
 from pyrogram import Client, filters
@@ -23,50 +23,75 @@ def on_pixiv(client: Client, message: Message):
     if message.chat.type == 'channel':
         if not client.get_chat_member(message.chat.id, 'me').can_post_messages:
             return
-    m = url_pattern.search(message.text.markdown or message.caption.markdown)
-    caption = m.string[m.end():]
-    m = re.search(r'\d+', m.string[m.start():m.end()])
+
+    m = url_pattern.search(message.text.markdown or message.caption.markdown)   # URL
+    caption = m.string[m.end():]    # Caption
+    m = re.search(r'\d+', m.string[m.start():m.end()])  # Artwork ID from URL
     artwork_id = int(m.string[m.start():m.end()])
+    m = re.search(r'\[((\d+,)+ )?\d+]', caption)    # Indexes
+    indexes: Union[List[int], None] = None
+    if m is not None:
+        indexes = eval(m.string[m.start():m.end()])
+        caption = m.string[:m.start()] + m.string[m.end():]
+
     illust = api.illust_detail(artwork_id)
+    caption = caption.replace('$TITLE', illust['illust']['title'])
     message.delete()
     if illust['illust']['meta_pages']:
         urls: List[str] = [meta_page['image_urls']['original'] for meta_page in illust['illust']['meta_pages']]
         filenames: List[str] = [url.split('/')[-1] for url in urls]
-        media: List[Union[InputMediaPhoto, InputMediaVideo, InputMediaAnimation, InputMediaDocument]] = []
+        media: Dict[str, List[Union[InputMediaPhoto, InputMediaVideo, InputMediaAnimation, InputMediaDocument]]] = {}
         for url in urls:
+            if indexes is not None and urls.index(url) not in indexes:
+                continue
             file_path = path.join(download_path, filenames[urls.index(url)])
             urllib.request.urlretrieve(url, file_path)
             if 'file' in message.text:
-                if len(media) == 0:
-                    media.append(InputMediaDocument(file_path, caption=caption))
+                if 'documents' not in media:
+                    media['documents'] = []
+                if len(media['documents']) == 0:
+                    media['documents'].append(InputMediaDocument(file_path, caption=caption))
                 else:
-                    media.append(InputMediaDocument(file_path))
+                    media['documents'].append(InputMediaDocument(file_path))
             else:
                 if filenames[urls.index(url)][-3:] in ('png', 'jpg', 'jpeg'):
-                    if len(media) == 0:
-                        media.append(InputMediaPhoto(file_path, caption=caption))
+                    if 'photos' not in media:
+                        media['photos'] = []
+                    if len(media['photos']) == 0:
+                        media['photos'].append(InputMediaPhoto(file_path, caption=caption))
                     else:
-                        media.append(InputMediaPhoto(file_path))
+                        media['photos'].append(InputMediaPhoto(file_path))
                 elif filenames[urls.index(url)][-3:] == 'gif':
-                    if len(media) == 0:
-                        media.append(InputMediaAnimation(file_path, caption=caption))
+                    if 'animations' not in media:
+                        media['animations'] = []
+                    if len(media['animations']) == 0:
+                        media['animations'].append(InputMediaAnimation(file_path, caption=caption))
                     else:
-                        media.append(InputMediaAnimation(file_path))
+                        media['animations'].append(InputMediaAnimation(file_path))
                 elif filenames[urls.index(url)][-3:] == 'mp4':
-                    if len(media) == 0:
-                        media.append(InputMediaVideo(file_path, caption=caption))
+                    if 'videos' not in media:
+                        media['videos'] = []
+                    if len(media['videos']) == 0:
+                        media['videos'].append(InputMediaVideo(file_path, caption=caption))
                     else:
-                        media.append(InputMediaVideo(file_path))
+                        media['videos'].append(InputMediaVideo(file_path))
                 else:
-                    if len(media) == 0:
-                        media.append(InputMediaDocument(file_path, caption=caption))
+                    if 'documents' not in media:
+                        media['documents'] = []
+                    if len(media['documents']) == 0:
+                        media['documents'].append(InputMediaDocument(file_path, caption=caption))
                     else:
-                        media.append(InputMediaDocument(file_path))
-        client.send_media_group(message.chat.id, media)
-        try:
-            [remove(path.join(download_path, filename)) for filename in filenames]
-        except OSError as e:
-            print(e)
+                        media['documents'].append(InputMediaDocument(file_path))
+        for media_type in media:
+            for i in range(len(media[media_type]) // 10):
+                client.send_media_group(message.chat.id, media[media_type][i*10:(i+1)*10])
+            if len(media[media_type]) % 10 > 0:
+                client.send_media_group(message.chat.id, media[media_type][-(len(media[media_type]) % 10):])
+        for filename in filenames:
+            try:
+                remove(path.join(download_path, filename))
+            except OSError:
+                continue
     elif illust['illust']['meta_single_page']:
         url = illust['illust']['meta_single_page']['original_image_url']
         filename = url.split('/')[-1]

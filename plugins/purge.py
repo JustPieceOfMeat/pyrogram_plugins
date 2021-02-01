61a3adef87c940a3f4e54daa6cb7143ebd39bf96
Alexey Razumov <alexey_razumov@tutanota.com>
2021-02-01 21:39:45 +0300

    Next time someone asks you how to fix an infinite loop, remember this commit

--- plugins/purge.py
+++ plugins/purge.py
@@ -2,7 +2,7 @@ from time import sleep
 
 from pyrogram import Client, filters
 from pyrogram.types import Message
-from pyrogram.errors import FloodWait
+from pyrogram.errors import FloodWait, RPCError
 
 
 @Client.on_message(filters.command('purge', ['.', '!']) & filters.me & filters.reply)
@@ -10,11 +10,8 @@ def purge(client: Client, message: Message):
     messages = client.iter_history(message.chat.id, offset_id=message.reply_to_message.message_id, reverse=True)
     for msg in messages:
         try:
-            if 'me' in message.command and not msg.from_user.is_self:
-                continue
-            client.delete_messages(msg.chat.id, msg.message_id)
+            message.delete()
         except FloodWait as e:
             sleep(e.x)
-        except Exception as e:
-            message.edit("Unexpected error, can't continue...\n\n" + e)
-            return
+        except RPCError as e:
+            continue

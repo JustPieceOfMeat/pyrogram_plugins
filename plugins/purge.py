57cfbadf9b990658b9fad620c7fde150d2024eb3
Alexey Razumov <alexey_razumov@tutanota.com>
2021-02-01 20:25:08 +0300

    This changes nothing, don't look

--- plugins/purge.py
+++ plugins/purge.py
@@ -8,11 +8,11 @@ from pyrogram.errors import FloodWait
 @Client.on_message(filters.command('purge', ['.', '!']) & filters.me & filters.reply)
 def purge(client: Client, message: Message):
     messages = client.iter_history(message.chat.id, offset_id=message.reply_to_message.message_id, reverse=True)
-    for message in messages:
+    for msg in messages:
         try:
-            if 'me' in message.command and not message.from_user.is_self:
+            if 'me' in message.command and not msg.from_user.is_self:
                 continue
-            client.delete_messages(message.chat.id, message.message_id)
+            client.delete_messages(msg.chat.id, msg.message_id)
         except FloodWait as e:
             sleep(e.x)
         except Exception as e:
